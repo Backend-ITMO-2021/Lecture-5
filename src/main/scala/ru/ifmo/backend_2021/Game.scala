c9e64d5c14afdc892c47b578f964f571f7dcc156
ad.tarasov <ad.tarasov@corp.mail.ru>
2021-04-16 21:48:09 +0300

    fix mutable

--- src/main/scala/ru/ifmo/backend_2021/Game.scala
+++ src/main/scala/ru/ifmo/backend_2021/Game.scala
@@ -6,56 +6,59 @@ import scala.util.control.Breaks.{break, breakable}
 
 class Game(private val defGrid: List[List[Int]]) {
 
-  private val grid: mutable.Seq[List[Int]] = ListBuffer.empty ++= defGrid
-
   def start(): Unit = {
-    println(SudokuUtils.renderSudoku(grid.toList))
-    while (!SudokuUtils.checkWin(grid.toList)) {
+    println(SudokuUtils.renderSudoku(defGrid))
+    play(defGrid)
+  }
+
+  private def play(grid: List[List[Int]]) {
+    if (!SudokuUtils.checkWin(grid)) {
       try {
-        breakable {
-          println("Введите x")
-          val x = scala.io.StdIn.readInt()
-          println("Введите y")
-          val y = scala.io.StdIn.readInt()
-          println("Введите v")
-          val v = scala.io.StdIn.readInt()
-
-          if (!makeStep(x, y, v)) break
-
-          println(SudokuUtils.renderSudoku(grid.toList))
-        }
+        println("Введите x")
+        val x = scala.io.StdIn.readInt()
+        println("Введите y")
+        val y = scala.io.StdIn.readInt()
+        println("Введите v")
+        val v = scala.io.StdIn.readInt()
+        val newGrid = makeStep(x, y, v, grid)
+        println(SudokuUtils.renderSudoku(newGrid))
+        play(newGrid)
       } catch {
         case _: NumberFormatException =>
           println("Неверный формат ввода")
+          play(grid)
       }
     }
     println("GG")
   }
 
-  private def makeStep(curX: Int, curY: Int, v: Int): Boolean ={
+  private def makeStep(curX: Int, curY: Int, v: Int, grid: List[List[Int]]): List[List[Int]] = {
     val correctRange = 1 to 9
 
     if (!(correctRange contains curX) || !(correctRange contains curY) || !(correctRange contains v)) {
       println("Неверные значения в границах")
-      return false
+      return grid
     }
 
     val x = curX - 1
     val y = curY - 1
 
-    if (defGrid(x)(y) != 0){
+    if (defGrid(x)(y) != 0) {
       println("Ячейка уже занята изначально")
-      return false
+      return grid
     }
     val prefValue = grid(x)(y)
-    grid(x) = grid(x).updated(y, v)
-    if (!SudokuUtils.isValidSudoku(grid.toList)){
-      grid(x) = grid(x).updated(y, prefValue)
+    val newGrid = createNewGrid(grid, x, y, v)
+    if (!SudokuUtils.isValidSudoku(newGrid)) {
+      val newGrid = createNewGrid(grid, x, y, prefValue)
       println("Неверный ход по правилам")
-      return false
+      return newGrid
     }
-
-    true
+    println(newGrid)
+    newGrid
   }
+
+  def createNewGrid(grid: List[List[Int]], x: Int, y: Int, v: Int): List[List[Int]] =
+    grid.patch(x, Seq(grid(x).patch(y, Seq(v), 1)), 1)
 
 }
